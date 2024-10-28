066429a37f56563ff7db6e2507e09c5bd3d56a61
Ricoroma <romansafronov0609@gmail.com>
2024-10-28 16:27:06 +0300

    init commit

---
+++ src/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, AsyncAttrs
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+from typing import AsyncGenerator
+from sqlalchemy import MetaData
+
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=naming_convention)
+Base = declarative_base(metadata=metadata, cls=(AsyncAttrs))
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL, poolclass=NullPool, echo=True, pool_pre_ping=True)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
---
+++ src/models.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum, DateTime, Table, UniqueConstraint, LargeBinary
+from sqlalchemy.orm import relationship
+from src.database import Base
